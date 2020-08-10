24b567da69e1626405848ca8a854548e3513c1f4
Arto Ranta <arto.ranta@polku.io>
2020-08-10 09:04:21 +0300

    fix generate

--- generate.js
+++ generate.js
@@ -15,23 +15,22 @@ const connector = require('./app/lib/connector');
 // Set directory.
 const tempDir = './temp';
 
-// Make sure directory exists.
-if (!fs.access(tempDir)) fs.mkdirSync(tempDir);
-
 // Set listener for completion of loading.
 connector.emitter.on('collections',
     // Write collections to file system in base64 format.
     async (collections) => {
-    for (let i = 0; i < Object.keys(collections).length; i++) {
-        try {
-            await fs.writeFile(tempDir + '/' + Object.keys(collections)[i].toUpperCase(),
-                Buffer.from(JSON.stringify(collections[Object.keys(collections)[i]])).toString('base64'),
-                'utf8');
-            console.log('Generated environment variable ' + Object.keys(collections)[i].toUpperCase() + '.');
-        } catch (err) {
-            console.log(err.message)
+        // Make sure directory exists.
+        await fs.access(tempDir).then(() => true).catch(async () => await fs.mkdir(tempDir));
+        for (let i = 0; i < Object.keys(collections).length; i++) {
+            try {
+                await fs.writeFile(tempDir + '/' + Object.keys(collections)[i].toUpperCase(),
+                    Buffer.from(JSON.stringify(collections[Object.keys(collections)[i]])).toString('base64'),
+                    'utf8');
+                console.log('Generated environment variable ' + Object.keys(collections)[i].toUpperCase() + '.');
+            } catch (err) {
+                console.log(err.message)
+            }
         }
-    }
-    // Exit.
-    process.exit();
-});
+        // Exit.
+        process.exit();
+    });
