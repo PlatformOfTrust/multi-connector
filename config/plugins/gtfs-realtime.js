5d8f85dcf9ae91dd5a83fa8311e9e32e8dcb581f
Arto Ranta <arto.ranta@polku.io>
2020-08-31 16:01:05 +0300

    gtfs fix

--- config/plugins/gtfs-realtime.js
+++ config/plugins/gtfs-realtime.js
@@ -3,13 +3,14 @@
  * Module dependencies.
  */
 const gtfs = require('gtfs-realtime-bindings');
+const _ = require('lodash');
 
 /**
  * Parses binary GTFS-realtime data feed into JavaScript objects.
  */
 
 /** Import platform of trust request definitions. */
-const {PARAMETERS, IDS} = require('../../config/definitions/request');
+const {IDS} = require('../../config/definitions/request');
 
 /**
  * Parses response as a FeedMessage (root type of the GTFS-realtime schema).
@@ -23,7 +24,7 @@ const response = async (config, response) => {
         // Parse response.
         response = gtfs.transit_realtime.FeedMessage.decode(response.body);
         // Pick requested ids from request parameters.
-        const ids = Object.hasOwnProperty.call(config[PARAMETERS], IDS) ? config[PARAMETERS][IDS].map(o => o.id.toString()) : []
+        const ids = _.get(config, IDS) ? _.get(config, IDS).map(o => o.id.toString()) : []
         // Filter out only requested entities.
         return response.entity.filter(e => ids.length === 0 || ids.includes(e.id.toString()));
     } catch (e) {
