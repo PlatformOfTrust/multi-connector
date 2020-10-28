32359ff70bd6deb1fbd3cfefd003d805fad98c6a
Miika Petäjäniemi <miika@holda.io>
2020-10-28 15:59:08 +0200

    fix formatting

--- app/lib/rsa.js
+++ app/lib/rsa.js
@@ -1,12 +1,12 @@
-'use strict'
+"use strict";
 /**
  * Module dependencies.
  */
-const _ = require('lodash')
-const crypto = require('crypto')
-const cache = require('../cache')
-const request = require('request')
-const winston = require('../../logger.js')
+const _ = require("lodash");
+const crypto = require("crypto");
+const cache = require("../cache");
+const request = require("request");
+const winston = require("../../logger.js");
 
 /**
  * RSA library.
@@ -16,64 +16,64 @@ const winston = require('../../logger.js')
 
 /** Platform of Trust related definitions. */
 const {
-	defaultKeySize,
-	publicKeyURLs,
-} = require('../../config/definitions/pot')
+    defaultKeySize,
+    publicKeyURLs,
+} = require("../../config/definitions/pot");
 
 /** Optional environment variables. */
-let privateKey = process.env.PRIVATE_KEY
-let publicKey = process.env.PUBLIC_KEY
+let privateKey = process.env.PRIVATE_KEY;
+let publicKey = process.env.PUBLIC_KEY;
 
 if (!privateKey || !publicKey) {
-	// If RSA keys are not provided by environment variables,
-	// they are generated on load with the default key size.
+    // If RSA keys are not provided by environment variables,
+    // they are generated on load with the default key size.
 
-	crypto.generateKeyPair(
-		'rsa',
-		{
-			modulusLength: defaultKeySize,
-			publicKeyEncoding: {
-				type: 'spki',
-				format: 'pem',
-			},
-			privateKeyEncoding: {
-				type: 'pkcs8',
-				format: 'pem',
-			},
-		},
-		(err, pubKey, privKey) => {
-			if (!err) {
-				privateKey = privKey
-				publicKey = pubKey
-				winston.log('info', 'Generated RSA keys.')
-			} else {
-				winston.log('error', err.message)
-			}
-		},
-	)
+    crypto.generateKeyPair(
+        "rsa",
+        {
+            modulusLength: defaultKeySize,
+            publicKeyEncoding: {
+                type: "spki",
+                format: "pem",
+            },
+            privateKeyEncoding: {
+                type: "pkcs8",
+                format: "pem",
+            },
+        },
+        (err, pubKey, privKey) => {
+            if (!err) {
+                privateKey = privKey;
+                publicKey = pubKey;
+                winston.log("info", "Generated RSA keys.");
+            } else {
+                winston.log("error", err.message);
+            }
+        }
+    );
 }
 
 /**
  * Reads public keys from Platform of Trust resources.
  */
 const readPublicKeys = function() {
-	for (let i = 0; i < publicKeyURLs.length; i++) {
-		request(publicKeyURLs[i].url, function(err, response, body) {
-			if (err) {
-				winston.log('error', err.message)
-			} else {
-				cache.setDoc('publicKeys', i, {
-					priority: i,
-					...publicKeyURLs[i],
-					key: body.toString(),
-				})
-			}
-		})
-	}
-}
+    for (let i = 0; i < publicKeyURLs.length; i++) {
+        request(publicKeyURLs[i].url, function(err, response, body) {
+            if (err) {
+                winston.log("error", err.message);
+            } else {
+                cache.setDoc("publicKeys", i, {
+                    priority: i,
+                    ...publicKeyURLs[i],
+                    key: body.toString(),
+                });
+            }
+        });
+    }
+};
 
 // Initiate public keys reading.
-readPublicKeys()
+readPublicKeys();
 
 /**
  * Sends public key response.
@@ -82,10 +82,10 @@ readPublicKeys()
  * @param {Object} res
  */
 const sendPublicKey = function(req, res) {
-	res.setHeader('Content-type', 'application/octet-stream')
-	res.setHeader('Content-disposition', 'attachment; filename=public.key')
-	res.send(publicKey)
-}
+    res.setHeader("Content-type", "application/octet-stream");
+    res.setHeader("Content-disposition", "attachment; filename=public.key");
+    res.send(publicKey);
+};
 
 /**
  * Sorts object recursively.
@@ -95,22 +95,24 @@ const sendPublicKey = function(req, res) {
  *   Sorted object.
  */
 const sortObject = function(object) {
-	let sortedObj = {}
-	Object.keys(object)
-		.sort()
-		.forEach((key) => {
-			if (
-				object[key] instanceof Array ||
-				typeof object[key] !== 'object'
-			) {
-				sortedObj[key] = object[key]
-				return
-			}
-			sortedObj[key] = sortObjectAlt(object[key])
-			return
-		}, {})
-	return sortedObj
-}
+    let sortedObj = {};
+
+    Object.keys(object)
+        .sort()
+        .forEach(key => {
+            if (
+                object[key] instanceof Array ||
+                typeof object[key] !== "object"
+            ) {
+                sortedObj[key] = object[key];
+                return;
+            }
+            sortedObj[key] = sortObjectAlt(object[key]);
+            return;
+        }, {});
+
+    return sortedObj;
+};
 
 /**
  * Stringifies body object.
@@ -120,16 +122,15 @@ const sortObject = function(object) {
  *   Stringified body.
  */
 const stringifyBody = function(body) {
-	// Stringify sorted object.
-	return JSON.stringify(sortObject(body))
-		.replace(
-			/[\u007F-\uFFFF]/g,
-			(chr) =>
-				'\\u' + ('0000' + chr.charCodeAt(0).toString(16)).substr(-4),
-		)
-		.replace(new RegExp('":', 'g'), '": ')
-		.trim()
-}
+    // Stringify sorted object.
+    return JSON.stringify(sortObject(body))
+        .replace(
+            /[\u007F-\uFFFF]/g,
+            chr => "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4)
+        )
+        .replace(new RegExp('":', "g"), '": ')
+        .trim();
+};
 
 /**
  * Generates signature object for given payload.
@@ -142,24 +143,24 @@ const stringifyBody = function(body) {
  *   The signature value.
  */
 const generateSignature = function(body, key) {
-	// Use local private key, if not given.
-	if (!key) key = privateKey
+    // Use local private key, if not given.
+    if (!key) key = privateKey;
 
-	// Initialize signature value.
-	let signatureValue
+    // Initialize signature value.
+    let signatureValue;
 
-	// Create SHA256 signature in base64 encoded format.
-	try {
-		signatureValue = crypto
-			.createSign('sha256')
-			.update(stringifyBody(body))
-			.sign(key.toString(), 'base64')
-	} catch (err) {
-		winston.log('error', err.message)
-	}
+    // Create SHA256 signature in base64 encoded format.
+    try {
+        signatureValue = crypto
+            .createSign("sha256")
+            .update(stringifyBody(body))
+            .sign(key.toString(), "base64");
+    } catch (err) {
+        winston.log("error", err.message);
+    }
 
-	return signatureValue
-}
+    return signatureValue;
+};
 
 /**
  * Validates signature for given payload.
@@ -174,24 +175,24 @@ const generateSignature = function(body, key) {
  *   True if signature is valid, false otherwise.
  */
 const verifySignature = function(body, signature, key) {
-	// Use local public key, if not given.
-	if (!key) key = publicKey
+    // Use local public key, if not given.
+    if (!key) key = publicKey;
 
-	// Initialize verifier.
-	const verifier = crypto.createVerify('sha256')
+    // Initialize verifier.
+    const verifier = crypto.createVerify("sha256");
 
-	// Update verifier.
-	verifier.update(stringifyBody(body))
+    // Update verifier.
+    verifier.update(stringifyBody(body));
 
-	// Verify base64 encoded SHA256 signature.
-	return verifier.verify(key, signature, 'base64')
-}
+    // Verify base64 encoded SHA256 signature.
+    return verifier.verify(key, signature, "base64");
+};
 
 /**
  * Expose library functions.
  */
 module.exports = {
-	generateSignature,
-	verifySignature,
-	sendPublicKey,
-}
+    generateSignature,
+    verifySignature,
+    sendPublicKey,
+};
