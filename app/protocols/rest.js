7aa12848cfb0a299444046a54f5909c6d4e09b03
artoranta <arto.ranta@polku.io>
2021-01-18 12:08:56 +0200

    update

--- app/protocols/rest.js
+++ app/protocols/rest.js
@@ -5,6 +5,7 @@
 const winston = require('../../logger.js');
 const response = require('../lib/response');
 const rp = require('request-promise');
+const _ = require('lodash');
 
 /**
  * REST library.
@@ -47,8 +48,14 @@ const getDataByOptions = async (config, options, path) => {
         if (options.query.length > 0) {
             queryString += '?';
             for (let i = 0; i < options.query.length; i++) {
-                queryString += Object.keys(options.query[i])[0] + '=' + Object.values(options.query[i])[0];
-                if (i !== (options.query.length - 1)) queryString += '&';
+                if (_.isObject(options.query[i])) {
+                    const entries = Object.entries(options.query[i]);
+                    entries.forEach(entry => queryString += entry[0] + '=' + entry[1] + '&');
+                }
+            }
+
+            if (queryString[queryString.length - 1] === '&') {
+                queryString = queryString.slice(0, -1);
             }
 
             // Check whether the URL already contains query entries.
