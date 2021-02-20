aca509fb4e3d3ef43684f87e795374eb07d2a9ee
artoranta <arto.ranta@polku.io>
2021-02-20 11:16:58 +0200

    enable auth

--- app/routes/translator/v1/fetch/index.js
+++ app/routes/translator/v1/fetch/index.js
@@ -13,7 +13,7 @@ module.exports = function (passport) {
     const auth = passport.authenticate(['signature'], {session: false});
 
     /** Platform of Trust fetch endpoint. */
-    router.post('', ctrl.fetch);
+    router.post('', auth, ctrl.fetch);
 
     return router;
 };
