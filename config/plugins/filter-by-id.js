1569e964caa06526a3046f0dde87bbddf99fb117
artoranta <arto.ranta@polku.io>
2021-04-01 08:48:05 +0300

    streamer plugin options

---
+++ config/plugins/filter-by-id.js
@@ -0,0 +1,39 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+const _ = require('lodash');
+
+/**
+ * Filtering plugin.
+ */
+
+/**
+ * Filters response object by id.
+ *
+ * @param {Object} config
+ * @param {Object} response
+ * @return {Object}
+ */
+const response = async (config, response) => {
+    try {
+        const ids = (_.get(config, 'parameters.ids') || []).map(object => object.id);
+        const idPath = 'generalConfig.hardwareId.dataObjectProperty';
+        const idKey = _.get(config, idPath);
+        config['dataObjects'].forEach((path) => {
+            // Filter objects by id.
+            response[path] = response[path].filter(object => ids.includes(_.get(object, idKey)));
+        });
+        return response;
+    } catch (e) {
+        return response;
+    }
+};
+
+/**
+ * Expose plugin methods.
+ */
+module.exports = {
+    name: 'filter-by-id',
+    response,
+};
---
+++ config/plugins/logger.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+ * logger plugin.
+ *
+ * Handy tool to debug the connector. Add to template plugins array and everything will be logged.
+ */
+const logger = function (method, config, param) {
+    console.log('\n<--- ' + method +' --->');
+    console.log(param);
+    return param;
+};
+
+/**
+ * Expose plugin methods.
+ */
+module.exports = {
+    name: 'logger',
+    template: (config, param) => logger('TEMPLATE', config, param),
+    parameters: (config, param) => logger('PARAMETERS', config, param),
+    request: (config, param) => logger('REQUEST', config, param),
+    response: (config, param) => logger('RESPONSE', config, param),
+    data: (config, param) => logger('DATA', config, param),
+    id: (config, param) => logger('ID', config, param),
+    output: (config, param) => logger('OUTPUT', config, param),
+};
