09a59e28910eedf73329e6566c88a0c7b4b3fab0
artoranta <arto.ranta@polku.io>
2021-06-04 14:24:50 +0300

    update airwits plugin

--- config/plugins/airwits.js
+++ config/plugins/airwits.js
@@ -16,9 +16,17 @@ const response = async (config, response) => {
     try {
         output.device = response.device;
         output.time = response.time * 1000;
-        output.header = response.data.substring(0, 2);
-        output.temp = parseInt(response.data.substring(2, 6), 16) / 10 - 40;
-        output.humidity = parseInt(response.data.substring(6, 8), 16);
+        switch (response.data.length) {
+            case 6:
+                output.temp = parseInt(response.data.substring(0, 4), 16) / 10 - 40;
+                output.humidity = parseInt(response.data.substring(4, 6), 16);
+                break;
+            case 8:
+                output.header = response.data.substring(0, 2);
+                output.temp = parseInt(response.data.substring(2, 6), 16) / 10 - 40;
+                output.humidity = parseInt(response.data.substring(6, 8), 16);
+        }
+
         return output;
     } catch (e) {
         return response;
