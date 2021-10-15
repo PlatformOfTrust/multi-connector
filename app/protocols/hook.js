511ff37577bd811bbb802b4c16c3f35ea12290f6
artoranta <arto.ranta@polku.io>
2021-10-15 09:09:58 +0300

    add apikey auth support to hook

--- app/protocols/hook.js
+++ app/protocols/hook.js
@@ -181,7 +181,7 @@ const controller = async (req, res) => {
     try {
         let topic = req.params.topic;
         const parts = req.originalUrl.split('/');
-        const productCode = parts.splice(parts.indexOf('hooks') + 1)[0];
+        const productCode = parts.splice(parts.indexOf('hooks') + 1)[0].split('?')[0];
         const config = cache.getDoc('configs', productCode) || {};
 
         if (!Object.hasOwnProperty.call(config, 'static')) {
@@ -190,15 +190,27 @@ const controller = async (req, res) => {
 
         /** Request authentication */
         const bearer = req.headers.authorization;
-        if (Object.hasOwnProperty.call(config.static, 'bearer') && bearer !== ('Bearer ' + config.static.bearer)) {
+        const apikey = req.query.apikey;
+
+        const bearerAuthEnabled = Object.hasOwnProperty.call(config.static, 'bearer');
+        const apikeyAuthEnabled = Object.hasOwnProperty.call(config.static, 'apikey');
+        const invalidBearer = bearer !== ('Bearer ' + config.static.bearer);
+        const invalidApikey = apikey !== config.static.apikey;
+
+        const authenticated =
+            (!bearerAuthEnabled && !apikeyAuthEnabled) ||
+            (bearerAuthEnabled && !invalidBearer) ||
+            (apikeyAuthEnabled && !invalidApikey);
+
+        if (!authenticated) {
             return res.status(401).send('Unauthorized');
         }
 
         try {
             // Pick topic/id from body (important with XML converted to JSON).
             if (!topic) {
-                const template = cache.getDoc('templates', config.template) || {};
-                topic = _.get(req.body, template.generalConfig.hardwareId.dataObjectProperty);
+                const template = cache.getDoc('templates', config.template) || {generalConfig: {}};
+                topic = _.get(req.body, (template.generalConfig.hardwareId || {}).dataObjectProperty);
             }
         } catch (err) {
             console.log(err.message);
