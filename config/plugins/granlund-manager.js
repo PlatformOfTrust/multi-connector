c65827aef01b87e544d501fc86f3ea7a02dbd1e2
artoranta <arto.ranta@polku.io>
2021-11-29 08:46:50 +0200

    gm schema

---
+++ config/plugins/granlund-manager.js
@@ -0,0 +1,103 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+const transformer = require('../../app/lib/transformer');
+const schema = require('../schemas/service-request_granlund-manager.json');
+
+/**
+ * Grandlund Manager transformer.
+ */
+
+
+/**
+ * Handles data objects.
+ *
+ * @param {Object} config
+ * @param {Object/String} id
+ * @param {Object} data
+ * @return {Object}
+ */
+const handleData = function (config, id, data) {
+    let object = {};
+    try {
+        const key = Object.keys(schema.properties.data.properties)[0];
+        for (let j = 0; j < data.length; j++) {
+            let result = {};
+            const value = data[j][config.output.value];
+
+            // Transform raw input.
+            value.type = 'Case';
+            result = transformer.transform(value, schema.properties.data);
+
+            // Merge all to same result.
+            if (Object.hasOwnProperty.call(object, key)) {
+                if (!Array.isArray(object[key])) {
+                    object[key] = [object[key]];
+                }
+                if (!Array.isArray(result[key])) {
+                    result[key] = [result[key]];
+                }
+                object[key].push(...result[key]);
+            } else {
+                object = result;
+            }
+        }
+        if (JSON.stringify(object) === '{}') {
+            object = {[key]: []};
+        }
+        return object;
+    } catch (err) {
+        return object;
+    }
+};
+
+/**
+ * Transforms output to Platform of Trust context schema.
+ *
+ * @param {Object} config
+ * @param {Object} output
+ * @return {Object}
+ */
+const output = async (config, output) => {
+    // Initialize harmonized output.
+    const result = {
+        [config.output.context]: config.output.contextValue,
+        [config.output.object]: {
+            [config.output.array]: [],
+        },
+    };
+
+    // Hand over data objects to transformer.
+    try {
+        const array = output.data[config.output.array];
+        for (let i = 0; i < array.length; i++) {
+            result[config.output.object][config.output.array].push(
+                handleData(
+                    config,
+                    array[i][config.output.id],
+                    array[i][config.output.data],
+                ));
+        }
+        if (result[config.output.object][config.output.array].length === 1) {
+            result[config.output.object] =
+                result[config.output.object][config.output.array][0];
+        } else {
+            result[config.output.object][config.output.array] =
+                result[config.output.object][config.output.array].map((o => {
+                    return Object.values(o);
+                })).flat(2);
+        }
+        return result;
+    } catch (err) {
+        return output;
+    }
+};
+
+/**
+ * Expose plugin methods.
+ */
+module.exports = {
+    name: 'granlund-manager',
+    output,
+};
