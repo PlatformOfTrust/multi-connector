84b1f33b85cc930aae7987cd40e88d00924b0295
artoranta <arto.ranta@polku.io>
2022-03-14 15:39:05 +0200

    add function handler

--- app/lib/transformer.js
+++ app/lib/transformer.js
@@ -11,6 +11,64 @@ const {replacer} = require('./utils');
  * Handles output transformation.
  */
 
+// Custom handlers.
+const functions = {
+    Date: {
+        getMonthName: date => ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'][date.getUTCMonth()],
+    },
+};
+
+/**
+ * Checks if value is millis timestamp.
+ *
+ * @param {String/Number} value
+ * @return {Boolean}
+ */
+const isMillisString = (value) => {
+    if (!Object.hasOwnProperty.call(value, 'length')) {
+        return false;
+    }
+    return value.length === 13 && !Number.isNaN(Number.parseInt(value));
+};
+
+/**
+ * Handles value with defined handler.
+ *
+ * @param {String/Date} value
+ * @param {String} f
+ * @return {String}
+ */
+const handle = (value, f) => {
+    try {
+        let method = f;
+        let args = [];
+        if (typeof f === 'object') {
+            if (Object.hasOwnProperty.call(f, 'method')) {
+                method = f.method;
+            }
+            if (Object.hasOwnProperty.call(f, 'args')) {
+                args = f.args;
+            }
+        }
+        const parts = method.split('.');
+        if (parts[0] === 'Date' && !(value instanceof Date)) {
+            value = value === '' ? new Date() : new Date(isMillisString(value) ? Number.parseInt(value) : value);
+        }
+        if (parts[0] === 'String' && !(value instanceof String)) {
+            value = value.toString();
+        }
+        if (typeof value[parts[1]] === 'function') {
+            value = value[parts[1]](...args);
+        } else {
+            value = _.get(functions, method)(value);
+        }
+        return value;
+    } catch (err) {
+        return value;
+    }
+};
+
 /**
  * Extracts the most inner placeholder.
  *
@@ -102,11 +160,16 @@ const transform = function (source, schema) {
                 case 'number':
                     if (Object.hasOwnProperty.call(schema, 'value')) {
                         value = replacePlaceholders(source, schema.value);
+                        if (Object.hasOwnProperty.call(schema, 'function')) {
+                            value = handle(value, schema.function);
+                        }
                     } else if (Object.hasOwnProperty.call(schema, 'source')) {
                         if (schema.source) {
                             value = schema.source === '' ? source : _.get(source, schema.source, schema.default);
                             if (value === '' && Object.hasOwnProperty.call(schema, 'default')) {
                                 value = schema.default;
+                            } else if (Object.hasOwnProperty.call(schema, 'function')) {
+                                value = handle(value, schema.function);
                             }
                         }
                     } else if (Object.hasOwnProperty.call(schema, 'const')) {
