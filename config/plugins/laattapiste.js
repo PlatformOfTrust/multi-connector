4d9701521d8b04d63325e4971ec17509cc485c21
artoranta <arto.ranta@polku.io>
2022-05-25 13:36:12 +0300

    compose reference

--- config/plugins/laattapiste.js
+++ config/plugins/laattapiste.js
@@ -824,6 +824,15 @@ const json2xml = (input = {}) => {
         {name: 'workPackageOperatorContactTelephone', value: _.get(input, 'addressShipping.contact.contactInformation.phoneNumber')},
     ];
 
+    // Compose reference.
+    if (input.reference === '') {
+        input.reference = [
+            _.get(input, 'addressShipping.process.idLocal'),
+            _.get(input, 'addressShipping.location.zone'),
+            _.get(input, 'addressShipping.location.space'),
+        ].join(' / ');
+    }
+
     let output;
     let xml;
     try {
