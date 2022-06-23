f8bc1915750712fce49aca8e26da266e244d558f
artoranta <arto.ranta@polku.io>
2022-06-23 14:54:34 +0300

    fix metec consumption calc

--- config/plugins/consumption.js
+++ config/plugins/consumption.js
@@ -10,12 +10,40 @@
  * @return {Object}
  */
 function findStartEnd (items) {
-    const result = {start: items[0], end: items[0]};
-    items.forEach(function (value) {
-        result.start = value.timestamp < result.start.timestamp ? value : result.start;
-        result.end = value.timestamp > result.end.timestamp ? value : result.end;
+    const result = {};
+    // Group by type.
+    items.forEach(item => {
+        const type = item['@type'] || 'N/A';
+        if (!Object.hasOwnProperty.call(result, type)) {
+            result[type] = {...item, data: [], startValue: item.value, endValue: item.value};
+        }
+        result[type].data.push(item);
+        result[type].data.sort((x, y) => new Date(x.timestamp).getTime() - new Date(y.timestamp).getTime());
+        if (new Date(item.timestamp).getTime() < new Date(result[type].start).getTime() || result[type].start === undefined) {
+            result[type].startValue = Number.parseFloat(item.value);
+            result[type].start = item.timestamp;
+        }
+        if (new Date(item.timestamp).getTime() > new Date(result[type].end).getTime() || result[type].end === undefined) {
+            result[type].endValue = Number.parseFloat(item.value);
+            result[type].end = item.timestamp;
+        }
     });
-    return {startValue: result.start.value, endValue: result.end.value, start: result.start.timestamp, end: result.end.timestamp};
+    // Calculate consumption.
+    Object.entries(result).forEach(([type, item]) => {
+        const peaks = [0];
+        let index = 0;
+        for (let i = 0; i < item.data.length; i++) {
+            if (Number.parseFloat(item.data[i].value) >= peaks[index]) {
+                peaks[index] = Number.parseFloat(item.data[i].value);
+            } else {
+                index++;
+                peaks[index] = Number.parseFloat(item.data[i].value);
+            }
+        }
+        result[type].value = peaks.reduce((a, c) => a + c);
+        result[type].value -= result[type].startValue;
+    });
+    return Object.values(result);
 }
 /**
  * Calculates start, end and consumption values.
@@ -28,16 +56,7 @@ const output = async (config, output) => {
     try {
         if ((config.schema || '').includes('measure-water-meter-reading')) {
             output.data.process = (output.data.process || output.data.sensors).map(sensor => {
-                const {startValue, endValue, start, end} = findStartEnd(sensor.measurements);
-                const value = endValue - startValue;
-                sensor.measurements = [{
-                    ...sensor.measurements[0],
-                    value,
-                    start,
-                    startValue,
-                    end,
-                    endValue,
-                }];
+                sensor.measurements = findStartEnd(sensor.measurements);
                 return sensor;
             });
         }
