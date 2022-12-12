4d8a960c741354830feed5a002dcd51f9756cf11
artoranta <arto.ranta@polku.io>
2022-12-12 15:03:43 +0200

    cron schedule support

--- config/plugins/scheduler.js
+++ config/plugins/scheduler.js
@@ -2,46 +2,60 @@
 /**
  * Module dependencies.
  */
-const connector = require('../../app/lib/connector');
+const _ = require('lodash');
+const cron = require('node-cron');
 const cache = require('../../app/cache');
+const streamer = require('./streamer');
+const winston = require('../../logger.js');
+const connector = require('../../app/lib/connector');
 
 /**
  * Scheduler.
  */
 
-const DEFAULT_INTERVAL = 10000;
+// Cron tasks.
+const tasks = {};
 const schedules = {};
+const DEFAULT_TIMEZONE = 'Europe/Helsinki';
+const DEFAULT_INTERVAL = 10000;
 
 /**
  * Initiates broker request.
  *
  * @param {String} productCode
  * @param {String} [mode]
+ * @param {Object} [options]
+ * @return {Object}
  */
-const brokerRequest = async (productCode, mode = 'latest') => {
+const brokerRequest = async (productCode, mode = 'latest', options) => {
     try {
-        const parameters = cache.getDoc('parameters', productCode);
-        if (parameters) {
-            // Detect mode from scheduler options.
-            if (mode === 'latest') {
-                delete parameters.start;
-                delete parameters.end;
+        const productCodes = Array.isArray(productCode) ? productCode : [productCode];
+        const result = {};
+        for (let i = 0; i < productCodes.length; i++) {
+            const parameters = cache.getDoc('parameters', options.id || productCodes[i]);
+            if (parameters) {
+                // Detect mode from scheduler options.
+                if (mode === 'latest') {
+                    delete parameters.start;
+                    delete parameters.end;
+                }
+                const triggeredReq = {
+                    body: {
+                        productCode: productCodes[i],
+                        timestamp: new Date().toISOString(),
+                        parameters,
+                    },
+                    connectorUrl: '',
+                    publicKeyUrl: '',
+                };
+                result[productCodes[i]] = await connector.getData(triggeredReq);
+            } else {
+                result[productCodes[i]] = null;
             }
-            const triggeredReq = {
-                body: {
-                    productCode,
-                    timestamp: new Date().toISOString(),
-                    parameters,
-                },
-                connectorUrl: '',
-                publicKeyUrl: '',
-            };
-            return await connector.getData(triggeredReq);
-        } else {
-            return Promise.resolve(null);
         }
+        return result;
     } catch (e) {
-        console.log(e.message);
+        winston.log('error', e.message);
     }
 };
 
@@ -56,17 +70,92 @@ const stream = async (config, scheduler, response) => {
     try {
         if (Object.hasOwnProperty.call(scheduler, 'streamer')) {
             if (Object.hasOwnProperty.call(scheduler.streamer, 'url')) {
-                let template = {
+                const template = {
                     config: {...config, plugins: {streamer: scheduler.streamer}},
-                    plugins: ['streamer'],
+                    plugins: [streamer],
                     output: {},
                 };
-                template = await connector.resolvePlugins(template);
                 await template.plugins.find(p => p.name === 'streamer').stream(template, response.output);
             }
         }
-    } catch (e) {
-        console.log(e.message);
+    } catch (err) {
+        winston.log('error', err.message);
+    }
+};
+
+/**
+ * Starts a cron task by product code.
+ *
+ * @param {String} id
+ * @param {Object} config
+ * @param {Object} parameters
+ */
+const startTask = (id, config, parameters) => {
+    try {
+        const callback = async (scheduler) => {
+            let result = null;
+            try {
+                if (Object.hasOwnProperty.call(config, 'productCode')) {
+                    // Trigger broker request.
+                    result = await brokerRequest(config.productCode, scheduler.mode, {
+                        id,
+                        clientSecret: scheduler.clientSecret,
+                        appAccessToken: scheduler.appAccessToken,
+                    });
+                }
+                if (Object.hasOwnProperty.call(scheduler, 'callback')) {
+                    // Execute callback.
+                    return scheduler.callback(config, scheduler, result);
+                } else if (_.isObject(result)) {
+                    // Stream result.
+                    for (let i = 0; i < Object.keys(result).length; i++) {
+                        if (result[Object.keys(result)[i]]) {
+                            await stream(config, scheduler, Object.values(result)[i]);
+                        }
+                    }
+                }
+            } catch (err) {
+                winston.log('error', err.message);
+            }
+        };
+        if (cron.validate(parameters.scheduler.schedule) && !parameters.scheduler.interval) {
+            const schedule = parameters.scheduler.schedule;
+            const timezone = parameters.scheduler.timezone || DEFAULT_TIMEZONE;
+            winston.log('info', `Start a job with id ${id} and schedule ${schedule} at ${timezone} timezone`);
+            tasks[id] = cron.schedule(schedule, () => callback(parameters.scheduler), {
+                scheduled: true,
+                timezone,
+            });
+        } else {
+            winston.log('info', `Start a job with id ${id} and interval ${parameters.scheduler.interval}`);
+            schedules[id] = setInterval(callback, Math.max(DEFAULT_INTERVAL, parameters.scheduler.interval), parameters.scheduler);
+        }
+    } catch (err) {
+        winston.log('error', err.message);
+    }
+};
+
+/**
+ * Destroys a cron task by product code.
+ *
+ * @param {String} productCode
+ */
+const removeTask = (productCode) => {
+    try {
+        if (Object.hasOwnProperty.call(tasks, productCode)) {
+            if (Object.hasOwnProperty.call(tasks[productCode], 'stop')) {
+                winston.log('info', `Stop a job with id ${productCode}`);
+                tasks[productCode].stop();
+            }
+            delete tasks[productCode];
+        }
+        if (Object.hasOwnProperty.call(schedules, productCode)) {
+            winston.log('info', `Stop a job with id ${productCode}`);
+            clearInterval(schedules[productCode]);
+            delete schedules[productCode];
+        }
+    } catch (err) {
+        winston.log('error', err.message);
     }
 };
 
@@ -77,30 +166,28 @@ const stream = async (config, scheduler, response) => {
  * @param {Object} parameters
  * @return {Object}
  */
-const parameters = async (config, parameters) => {
+const parameters = (config, parameters) => {
     try {
-        if (Object.hasOwnProperty.call(parameters, 'scheduler')
-            && Object.hasOwnProperty.call(config, 'productCode')) {
+        if (Object.hasOwnProperty.call(parameters, 'scheduler') &&
+            (Object.hasOwnProperty.call(config, 'productCode') || Object.hasOwnProperty.call(config, 'id'))) {
             if (Object.hasOwnProperty.call(parameters.scheduler, 'interval')) {
-                // Remove previous schedule.
-                if (Object.hasOwnProperty.call(schedules, config.productCode)) {
-                    clearInterval(schedules[config.productCode]);
+                let id = config.productCode;
+                // Set custom id.
+                if (Object.hasOwnProperty.call(config, 'id')) {
+                    id = config.id;
                 }
+                // Remove previous schedule.
+                removeTask(id);
                 // Store parameters to cache.
-                cache.setDoc('parameters', config.productCode, {...parameters, scheduler: undefined});
+                cache.setDoc('parameters', id, {...parameters, scheduler: undefined});
                 // Set new schedule if provided interval is valid.
-                if (Number.isInteger(parameters.scheduler.interval)) {
-                    schedules[config.productCode] = setInterval(async (scheduler) => {
-                        // Trigger broker request.
-                        const result = await brokerRequest(config.productCode, scheduler.mode);
-                        // Stream result.
-                        if (result) await stream(config, scheduler, result);
-                    }, Math.max(DEFAULT_INTERVAL, parameters.scheduler.interval), parameters.scheduler);
+                if (Number.isInteger(parameters.scheduler.interval) || typeof parameters.scheduler.schedule === 'string') {
+                    startTask(id, config, parameters);
                 }
             }
         }
-    } catch (e) {
-        console.log(e.message);
+    } catch (err) {
+        winston.log('error', err.message);
     }
     return parameters;
 };
