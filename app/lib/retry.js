a45979691f97146c6d09f40cc5298ad6a31101e5
artoranta <arto.ranta@polku.io>
2023-05-05 16:49:12 +0300

    retry library

---
+++ app/lib/retry.js
@@ -0,0 +1,92 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+const winston = require('../../logger.js');
+const {getData, sendData, remove} = require('../../app/protocols/azure-blob-storage');
+
+const container = 'retries';
+
+const upload = async (id, name, content, metadata = {attempts: '1'}) => {
+    const filename = name.split('/')[0] === '' ? name.substring(1) : name;
+    // Upload file to blob storage.
+    const result = await sendData({
+        authConfig: {
+            account: process.env.AZURE_BLOB_STORAGE_ACCOUNT,
+            accountKey: process.env.AZURE_BLOB_STORAGE_ACCOUNT_KEY,
+            container,
+        },
+    }, [{filename: `${id}${id ? '/' : ''}${filename}`, content, metadata}], true);
+    if (result.length > 0) {
+        return Promise.resolve(result);
+    } else {
+        return Promise.reject();
+    }
+};
+
+const download = async (id, name = '') => {
+    const filename = name.split('/')[0] === '' ? name.substring(1) : name;
+    return await getData({
+        authConfig: {
+            account: process.env.AZURE_BLOB_STORAGE_ACCOUNT,
+            accountKey: process.env.AZURE_BLOB_STORAGE_ACCOUNT_KEY,
+            container,
+        },
+        parameters: {
+            targetObject: {},
+        },
+    }, [`${id}${id ? '/' : ''}${filename}`], true);
+};
+
+const retry = async (id, callback) => {
+    // Check for blobs and retry.
+    download(id).then(async (file) => {
+        // Try again.
+        for (let i = 0; i < file.length; i++) {
+            const attemptValue = ((file[i] || {}).metadata || {}).attempts || '1';
+            let attemptCount = !Number.isNaN(Number.parseInt(attemptValue)) ? Number.parseInt(attemptValue) : 1;
+            if (attemptCount >= 5) {
+                continue;
+            }
+            const success = await callback(file[i]);
+            if (success) {
+                // Remove file as callback was successful.
+                await remove({
+                    authConfig: {
+                        account: process.env.AZURE_BLOB_STORAGE_ACCOUNT,
+                        accountKey: process.env.AZURE_BLOB_STORAGE_ACCOUNT_KEY,
+                        container,
+                    },
+                }, [file[i].id]);
+            } else {
+                // Increase attempt count as callback failed.
+                attemptCount += 1;
+                await upload('', file[i].id, Buffer.from(file[i].data, 'base64'), {attempts: attemptCount.toString()});
+            }
+        }
+    }).catch(err => {
+        winston.log('error', err.message);
+    });
+};
+
+/**
+ * Handles retry.
+ *
+ * @param {String} id
+ * @param {Object} input
+ * @param {Function} callback
+ */
+const add = async (id, input, callback) => {
+    upload(id, input.path, Buffer.from(input.data, 'base64')).then(() => {
+        // Confirm successful move to blob storage by executing the callback.
+        callback();
+    }).catch(err => {
+        winston.log('error', err.message);
+    });
+};
+
+module.exports = {
+    add,
+    get: async (name) => download('', name),
+    retry,
+};
